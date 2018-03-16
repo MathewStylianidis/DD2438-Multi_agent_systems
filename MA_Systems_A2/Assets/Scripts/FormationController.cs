886b0642ea805688a756db5ef653da6d7ce199a4
MattSt <mattstyl@outlook.com>
2018-03-16 15:30:23 +0100

    Small fix in formation followers orientation

--- MA_Systems_A2/Assets/Scripts/FormationController.cs
+++ MA_Systems_A2/Assets/Scripts/FormationController.cs
@@ -21,7 +21,7 @@ public class FormationController : MonoBehaviour {
 	void Update () {
 		this.desiredRelativePositions = getDesiredPositions (false);
 		this.desiredAbsolutePositions = getDesiredPositions ();
-		Visualizer.visualizePoints(this.desiredAbsolutePositions);
+		//Visualizer.visualizePoints(this.desiredAbsolutePositions);
 	}
 
 	public Vector2 getDesiredPosition(int agentIdx, bool absolute = true) {
@@ -92,7 +92,7 @@ public class FormationController : MonoBehaviour {
 	public Vector3[] getTrajectory() { return this.trajectory;	}
 	public float[] getTrajectoryOrientation() { return this.trajectoryOrientation; }
 	public float[] getTrajectoryTimestamps() { return this.trajectoryTimestamps; }
-	public Vector3 getLeaderOrientation() { return UtilityClass.rads2Vec (getLeaderRotation());}
-	public float getLeaderRotation() { return agents [0].transform.rotation.eulerAngles.y * Mathf.Deg2Rad; }
+	public Vector3 getLeaderOrientation() { Debug.Log (90.0f - agents [0].transform.rotation.eulerAngles.y);  return UtilityClass.rads2Vec (getLeaderRotation());}
+	public float getLeaderRotation() { return (90.0f - agents [0].transform.rotation.eulerAngles.y) * Mathf.Deg2Rad; }
 
 }
